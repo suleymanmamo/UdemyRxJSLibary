3595179754c74ab4b35841873346cecd71aa0d55
süleyman meşe <83617946+suleymanmamo@users.noreply.github.com>
2021-09-21 12:57:15 +0300

    ajax operatörü kullanımı

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from "@angular/core";
-import { defer, of, timer } from "rxjs";
-
+import { ajax } from "rxjs/ajax";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -8,20 +7,16 @@ import { defer, of, timer } from "rxjs";
 })
 export class AppComponent {
   title = "UdemyRxJSLibary";
-  subscription;
 
   constructor() {
-    const map = new Map();
-    map.set(1, "Kitaplar");
-    map.set(2, "Kalem");
-    map.set(3, "Silgiler");
-
-    const publisher = of(new Date());
-    const publisher2 = defer(() => of(new Date()));
-    const timer1 = timer(3000);
-    timer1.subscribe((data) => {
-      publisher.subscribe((val1) => console.log("of => " + val1));
-      publisher2.subscribe((val2) => console.log("defer => " + val2));
-    });
+    ajax.getJSON("https://jsonplaceholder.typicode.com/todos/5").subscribe(
+      (data) => {
+        console.table(data);
+      },
+      (err) => {},
+      () => {
+        console.log("ajax isteği bitti");
+      }
+    );
   }
 }
