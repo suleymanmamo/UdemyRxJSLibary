debf7dfa1f5833e8fc9930df22a9925fda6a5659
süleyman meşe <83617946+suleymanmamo@users.noreply.github.com>
2021-09-21 13:40:57 +0300

    pipe/first metodu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
-import { fromEvent } from "rxjs";
+import { Component } from "@angular/core";
+import { from } from "rxjs";
+import { first } from "rxjs/operators";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = "UdemyRxJSLibary";
+  constructor() {
+    const myarray = from([
+      5, 10, 15, 20, 50, 100, 200, 300, 350, 400, 750, 800,
+    ]);
 
-  @ViewChild("btn")
-  button: ElementRef;
-  constructor() {}
-  ngAfterViewInit(): void {
-    fromEvent(this.button.nativeElement, "click").subscribe((data) => {
+    myarray.pipe(first((val) => val > 15)).subscribe((data) => {
       console.log(data);
     });
   }
