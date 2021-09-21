25dae63b73e7bd0be0460c5c447b79dfa1c5afdd
süleyman meşe <83617946+suleymanmamo@users.noreply.github.com>
2021-09-21 12:38:32 +0300

    from operatörü kullanımı

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from "@angular/core";
-
-// import { of } from "rxjs"; \\
-// import { interval } from "rxjs"; \\
-// import { timer } from "rxjs";
-import { range } from "rxjs";
+import { from } from "rxjs";
 
 @Component({
   selector: "app-root",
@@ -15,55 +11,15 @@ export class AppComponent {
   subscription;
 
   constructor() {
-    //bölüm 3:3 \\
-    // const values = of("Fatih", 4, [1, 2, 3], "Sileman", 4.12);
-    // values.subscribe((data) => {
-    //   console.log(data);
-    // });
-    //***********************************************\\
-    //  bölüm 3:4\\
-    // const publisher = interval(10000);
-    // publisher.subscribe(
-    //   (val) => {
-    //     console.log("dinya" + val);
-    //   },
-    //   (err) => {},
-    //   () => {
-    //     // console.log("veri alma tamamlandı");
-    //   }
-    // );
-    //***********************************************\\
-    // bölüm 3:5/1 \\
-    // 2 saniye sonra tek işlem yapar
-    // const publisher = timer(2000);
-    // publisher.subscribe(
-    //   (data) => {
-    //     console.log("bu yazı 2 saniye sonra çalışacaktır");
-    //   },
-    //   (err) => {},
-    //   () => {
-    //     console.log("veri alma tamamlandı");
-    //   }
-    // );
-    // bölüm 3:5/2 \\
-    // 5 saniye sonra 2 saniyede bir değer tekrarlar
-    // const publisher = timer(3000, 2000);
-    // this.subscription = publisher.subscribe(
-    //   (val) => {
-    //     console.log("bu yazı 5 saniye sonra çalışacaktır = " + val);
-    //   },
-    //   (err) => {},
-    //   () => {
-    //     console.log("veri alma tamamlandı");
-    //   }
-    // );
-    //***********************************************\\
-    // bölüm 3:6
+    const map = new Map();
+    map.set(1, "Kitaplar");
+    map.set(2, "Kalem");
+    map.set(3, "Silgiler");
 
-    const publisher = range(1, 20);
+    const publisher = from(map);
     this.subscription = publisher.subscribe(
       (val) => {
-        console.log("gelen değer = " + val);
+        console.log(`${val[0]} = ${val[1]}`);
       },
       (err) => {},
       () => {
