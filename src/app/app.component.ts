e51833d10804afa616b4d62126166dbb5c8730b3
süleyman meşe <83617946+suleymanmamo@users.noreply.github.com>
2021-09-21 12:22:05 +0300

    timer operatörü kullanımı

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
-import { interval } from "rxjs";
+import { timer } from "rxjs";
 // import { of } from "rxjs"; \\
+// import { interval } from "rxjs"; \\
 
 @Component({
   selector: "app-root",
@@ -9,22 +10,53 @@ import { interval } from "rxjs";
 })
 export class AppComponent {
   title = "UdemyRxJSLibary";
+  subscription;
 
   constructor() {
+    //bölüm 3:3 \\
     // const values = of("Fatih", 4, [1, 2, 3], "Sileman", 4.12);
     // values.subscribe((data) => {
     //   console.log(data);
-    // }); bölüm 3:3
-
-    const publisher = interval(10000);
-    publisher.subscribe(
+    // });
+    //***********************************************\\
+    //  bölüm 3:4\\
+    // const publisher = interval(10000);
+    // publisher.subscribe(
+    //   (val) => {
+    //     console.log("dinya" + val);
+    //   },
+    //   (err) => {},
+    //   () => {
+    //     // console.log("veri alma tamamlandı");
+    //   }
+    // );
+    //***********************************************\\
+    // bölüm 3:5\\
+    // 2 saniye sonra tek işlem yapar
+    // const publisher = timer(2000);
+    // publisher.subscribe(
+    //   (data) => {
+    //     console.log("bu yazı 2 saniye sonra çalışacaktır");
+    //   },
+    //   (err) => {},
+    //   () => {
+    //     console.log("veri alma tamamlandı");
+    //   }
+    // );
+    // 5 saniye sonra 2 saniyede bir değer tekrarlar
+    const publisher = timer(3000, 2000);
+    this.subscription = publisher.subscribe(
       (val) => {
-        console.log("dinya" + val);
+        console.log("bu yazı 5 saniye sonra çalışacaktır = " + val);
       },
       (err) => {},
       () => {
-        // console.log("veri alma tamamlandı");
+        console.log("veri alma tamamlandı");
       }
     );
+  }
+
+  stop() {
+    this.subscription.unsubscribe();
   }
 }
