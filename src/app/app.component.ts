a558b27b5544dda0c447e9367172ceef3c9e5ac4
süleyman meşe <83617946+suleymanmamo@users.noreply.github.com>
2021-09-21 14:44:14 +0300

    distinct operatörü

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { from } from "rxjs";
-import { single } from "rxjs/operators";
+import { distinct } from "rxjs/operators";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -9,9 +9,11 @@ import { single } from "rxjs/operators";
 export class AppComponent {
   title = "UdemyRxJSLibary";
   constructor() {
-    const myarray = from(["ahmet", "halil", "celil"]);
+    const myarray = from([
+      5, 10, 15, 20, 50, 100, 200, 300, 350, 400, 750, 800, 300, 10, 50, 350,
+    ]);
 
-    myarray.pipe(single((val) => val == "celil")).subscribe(
+    myarray.pipe(distinct()).subscribe(
       (data) => {
         console.log(data);
       },
