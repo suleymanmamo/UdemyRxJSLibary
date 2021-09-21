542a2c2ce7f38fef178ba518c24da318671812e8
süleyman meşe <83617946+suleymanmamo@users.noreply.github.com>
2021-09-21 13:04:58 +0300

    create operatörü kullanımı

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { ajax } from "rxjs/ajax";
+import { Observable } from "rxjs";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -9,13 +9,27 @@ export class AppComponent {
   title = "UdemyRxJSLibary";
 
   constructor() {
-    ajax.getJSON("https://jsonplaceholder.typicode.com/todos/5").subscribe(
+    const myobservable = Observable.create((observer) => {
+      observer.next("sileman e");
+      observer.next("rih u cane");
+      observer.complete();
+    });
+    myobservable.subscribe(
       (data) => {
-        console.table(data);
+        console.log(data);
       },
       (err) => {},
       () => {
-        console.log("ajax isteği bitti");
+        console.log("data yayımı bitti şimdi reklamlar");
+      }
+    );
+    myobservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (err) => {},
+      () => {
+        console.log("data yayımı bitti şimdi reklamlar");
       }
     );
   }
