4996bfbfe0c55d70e3a45eea201e27980392968d
süleyman meşe <83617946+suleymanmamo@users.noreply.github.com>
2021-09-21 17:15:58 +0300

    take operatör

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { from } from "rxjs";
-import { skipWhile } from "rxjs/operators";
+import { skipWhile, take } from "rxjs/operators";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -11,7 +11,7 @@ export class AppComponent {
 
   constructor() {
     from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 17, 18, 19])
-      .pipe(skipWhile((x) => x < 5))
+      .pipe(take(7))
       .subscribe((data) => {
         console.log(data);
       });
