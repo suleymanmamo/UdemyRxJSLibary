b4966f3f7ac39bc77d4f1494b6ac277d2d8b077f
süleyman meşe <83617946+suleymanmamo@users.noreply.github.com>
2021-09-21 11:43:54 +0300

    of operatörü kullanımı

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { of } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'UdemyRxJSLibary';
+  title = "UdemyRxJSLibary";
+
+  constructor() {
+    const values = of("Fatih", 4, [1, 2, 3], "Sileman", 4.12);
+    values.subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
