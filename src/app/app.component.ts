1116ef403fef523f93ab34cd96d93712dd61a6f2
süleyman meşe <83617946+suleymanmamo@users.noreply.github.com>
2021-09-21 17:18:02 +0300

    takeLast operatör

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { from } from "rxjs";
-import { skipWhile, take } from "rxjs/operators";
+import { takeLast } from "rxjs/operators";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -10,8 +10,8 @@ export class AppComponent {
   title = "UdemyRxJSLibary";
 
   constructor() {
-    from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 17, 18, 19])
-      .pipe(take(7))
+    from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+      .pipe(takeLast(7))
       .subscribe((data) => {
         console.log(data);
       });
