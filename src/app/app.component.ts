7b9f503c7a1c945e53a53c77fed5318b7faed8e5
süleyman meşe <83617946+suleymanmamo@users.noreply.github.com>
2021-09-22 13:51:52 +0300

    map operatörü

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
-import { interval } from "rxjs";
-import { throttleTime } from "rxjs/operators";
+import { from } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-root",
@@ -11,10 +11,20 @@ export class AppComponent {
   title = "UdemyRxJSLibary";
 
   constructor() {
-    interval(1000)
-      .pipe(throttleTime(5000))
-      .subscribe((data) => {
-        console.log(data);
-      });
+    // const numbers = from([85, 23, 57, 36, 12, 68, 5, 18, 17, 25, 24, 68]);
+    // numbers.pipe(map((val) => val + 10)).subscribe((data) => {
+    //   console.log(data);
+    // });
+
+    const users = from([
+      { name: "cabbar", surname: "kökdal", email: "cabbar@gmail.com" },
+      { name: "ali hıdır", surname: "meşe", email: "ali.hidir@gmail.com" },
+      { name: "arthur", surname: "doyle", email: "arthurdoyle@gmail.com" },
+      { name: "ahmet arif", surname: "meşe", email: "ahmedarifmese@gmail.com" },
+      { name: "sileman", surname: "mamo", email: "adar.suleyman@gmail.com" },
+    ]);
+    users.pipe(map((user) => user.name)).subscribe((data) => {
+      console.log(data);
+    });
   }
 }
